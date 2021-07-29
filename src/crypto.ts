cdf2422a10144f0a927fdb92a19de51534de7cc2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-07-29 22:10:23 +0200

    feature(core): Improve crypto loading to support older browsers for the core package.

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,12 +1,31 @@
-// Isomorphic pattern to load Web Crypto in both browsers and Node.js
+/**
+ * Get a reference to the Web Crypto API in any modern JS environment
+ *
+ * @returns An object implementing the Web Crypto API.
+ */
 async function loadCrypto(): Promise<Crypto> {
-    if (globalThis && globalThis.crypto) {
-        // Running in browsers from 2018 and newer.
-        return new Promise((resolve, _reject) => resolve(globalThis.crypto))
+    if ((window && window.crypto) || (globalThis && globalThis.crypto)) {
+        // Running in browsers released after 2014, and other
+        // runtimes with `globalThis` like Deno or CloudFlare Workers
+        const subtle =
+            window.crypto.subtle ||
+            globalThis.crypto.subtle ||
+            (
+                window.crypto as unknown as {
+                    webkitSubtle: Crypto['subtle']
+                }
+            )?.webkitSubtle
+
+        return new Promise((resolve) =>
+            resolve({
+                getRandomValues: window.crypto.getRandomValues,
+                subtle,
+            }),
+        )
     } else {
         // Running in Node.js >= 15
-        const cryptoLocal = await import('crypto')
-        return ((cryptoLocal.webcrypto as unknown) as Crypto)
+        const nodeCrypto = await import('crypto')
+        return nodeCrypto.webcrypto as unknown as Crypto
     }
 }
 
