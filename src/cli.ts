82a14e883363c01c1b0b0a2b6081da631e1d8742
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-27 16:26:48 +0100

    Add support for password iterations in the CLI

--- src/cli.ts
+++ src/cli.ts
@@ -18,14 +18,23 @@ sade(`${name} <src> <dest> [password]`, true)
         '-g, --generate-password',
         'Generate a random password with given length. Must be a number if used.',
     )
+    .option('-i, --iterations', 'The number of password iterations.')
     .action(async (src, dest, password, options) => {
         const length = options['generate-password']
+        const iterations = options['iterations']
+
+        if (iterations !== undefined && !Number.isInteger(iterations)) {
+            console.error(
+                '❌: The option --iterations (-i) must be an integer.',
+            )
+            process.exit(1)
+        }
 
         if (length) {
             if (Number.isInteger(length)) {
                 const pass = generatePassword(length)
                 console.log(`🔐 Encrypting ${src} → ${dest} with 🔑: ${pass}`)
-                await encrypt(src, dest, pass)
+                await encrypt(src, dest, pass, iterations)
             } else {
                 console.error(
                     '❌: The <length> must be an integer when using --generate-password <length>',
@@ -34,7 +43,7 @@ sade(`${name} <src> <dest> [password]`, true)
             }
         } else if (password) {
             console.log(`🔐 Encrypting ${src} → ${dest} with 🔑: ${password}`)
-            await encrypt(src, dest, password)
+            await encrypt(src, dest, password, iterations)
         } else {
             console.error(
                 '❌: Either provide a password or use --generate-password <length>',
