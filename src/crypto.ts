8bc6cdfd4d91dfb8e668e407545f1c8153c726f9
Ricardo Jorge Afonso Catalão <ricardojorge512@hotmail.com>
2023-05-19 13:58:12 +0100

    Changed order to prefer globalThis over window.

--- src/crypto.ts
+++ src/crypto.ts
@@ -4,15 +4,15 @@
  * @returns An object implementing the Web Crypto API.
  */
 async function loadCrypto(): Promise<Crypto> {
-    if (typeof window !== 'undefined' && window.crypto) {
-        const crypto = window.crypto
-        return new Promise((resolve) => resolve(crypto))
-    } else if (globalThis && globalThis.crypto) {
+    if (globalThis && globalThis.crypto) {
 
         // Running in browsers released after 2017, and other
         // runtimes with `globalThis` like Deno or CloudFlare Workers
         const crypto = globalThis.crypto
 
+        return new Promise((resolve) => resolve(crypto))
+    } else if (typeof window !== 'undefined' && window.crypto) {
+        const crypto = window.crypto
         return new Promise((resolve) => resolve(crypto))
     } else {
         const nodeCrypto = await import('crypto')
