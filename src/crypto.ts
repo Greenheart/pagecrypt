25dfa2be9356cd6888866883e12efaa25c53dd96
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-01-17 01:29:33 +0100

    Use optional chaining

--- src/crypto.ts
+++ src/crypto.ts
@@ -4,10 +4,10 @@
  * @returns An object implementing the Web Crypto API.
  */
 async function loadCrypto(): Promise<Crypto> {
-    if (globalThis && globalThis.crypto) {
+    if (globalThis?.crypto) {
         // `globalThis` exists in modern browsers and runtimes like Deno, Bun or CloudFlare Workers
         return globalThis.crypto
-    } else if (typeof window !== 'undefined' && window.crypto) {
+    } else if (window?.crypto) {
         // Some older browsers released after 2017 only expose the crypto API via the window.
         return window.crypto
     } else {
